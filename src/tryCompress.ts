bad71f6dd162e30855361e87a2015a3cb64b1c37
EnergyPatrikHU <energyhun24@gmail.com>
2024-03-18 13:00:55 +0100

    Refactor tryCompress function to use async/await

--- src/tryCompress.ts
+++ src/tryCompress.ts
@@ -1,40 +1,28 @@
-import sharp, { OutputInfo } from 'sharp';
+import sharp from 'sharp';
 
-export function tryCompress(originalImageBuffer: Buffer, params: any, mediaSize: number) {
-	return new Promise<{ buffer: Buffer; size: number; quality: number }>((resolve) => {
-		sharp(originalImageBuffer, {
-			animated: params.format === 'webp',
+export async function tryCompress(originalImageBuffer: Buffer, params: any) {
+	try {
+		const { format, grayscale, quality } = params;
+
+		const outputInfo = await sharp(originalImageBuffer, {
+			animated: format === 'webp',
 			limitInputPixels: false,
 			failOn: 'none',
 			unlimited: true,
 		})
-			.grayscale(params.grayscale as any)
-			.toFormat(params.format as any, {
-				quality: params.quality as any,
+			.grayscale(grayscale as any)
+			.toFormat(format as any, {
+				quality: quality as any,
 				effort: 6,
 				mozjpeg: true,
 				progressive: true,
 				optimiseScans: true,
 				optimiseCoding: true,
 			})
-			.toBuffer(async function (error: Error, buffer: Buffer, outputInfo: OutputInfo) {
-				if (error) {
-					buffer.set([]);
-					buffer.fill(0);
-					buffer = null;
+			.toBuffer();
 
-					throw error ?? new Error('this should not happen');
-				}
-
-				// if (mediaSize - outputInfo.size < 0) {
-				// 	buffer.set([]);
-				// 	buffer.fill(0);
-				// 	buffer = null;
-
-				// 	return resolve(await tryCompress(originalImageBuffer, { ...params, quality: params.quality - 5 }, mediaSize));
-				// }
-
-				return resolve({ buffer: buffer, size: outputInfo.size!, quality: params.quality });
-			});
-	});
+		return { buffer: outputInfo, size: outputInfo.length!, quality };
+	} catch (error) {
+		throw error ?? new Error('This should not happen');
+	}
 }
