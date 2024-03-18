50407ae68be2d8969b58cd8af25bf64ef25cad58
EnergyPatrikHU <energyhun24@gmail.com>
2024-03-18 13:00:50 +0100

    Refactor redirect function to use response.writeHead() method

--- src/redirect.ts
+++ src/redirect.ts
@@ -3,19 +3,11 @@ import { IncomingMessage, ServerResponse } from 'http';
 export function redirect({ request, response, params }: { request: IncomingMessage; response: ServerResponse<IncomingMessage>; params: any }) {
 	if (response.headersSent) return;
 
-	response.setHeader('content-length', 0);
-	response.removeHeader('cache-control');
-	response.removeHeader('expires');
-	response.removeHeader('date');
-	response.removeHeader('etag');
-	response.setHeader('location', encodeURI(params.url));
-	response.statusCode = 302;
+	response.writeHead(302, {
+		'Content-Length': '0',
+		'Location': encodeURI(params.url),
+	});
 
 	response.end();
-	response.flushHeaders();
-	response.destroy();
-	response = null;
-	request.drop(Infinity);
 	request.destroy();
-	request = null;
 }
