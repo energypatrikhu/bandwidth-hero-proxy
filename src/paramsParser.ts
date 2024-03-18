a45c31e5fb8db25983a6b60414e87505dca83621
EnergyPatrikHU <energyhun24@gmail.com>
2024-03-18 13:00:36 +0100

    Refactor paramsParser function to handle array input for url parameter

--- src/paramsParser.ts
+++ src/paramsParser.ts
@@ -3,13 +3,18 @@ import { NextFunction, Request, Response } from 'express';
 export function paramsParser(request: Request, response: Response, next: NextFunction) {
 	let url = (request.query.url ?? '').toString();
 
-	if (Array.isArray(url)) url = url.join('&url=');
-	if (!url) return response.send('bandwidth-hero-proxy');
+	if (Array.isArray(url)) {
+		url = url.join('&url=');
+	}
+
+	if (!url) {
+		return response.send('bandwidth-hero-proxy');
+	}
 
 	url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
 	request.params.url = url;
-	request.params.format = request.query.jpg != '1' ? 'webp' : 'jpeg';
-	request.params.grayscale = (request.query.bw != '0') as any;
+	request.params.format = request.query.jpg !== '1' ? 'webp' : 'jpeg';
+	request.params.grayscale = (request.query.bw !== '0') as any;
 	request.params.quality = parseInt(request.query.l.toString()) as any;
 
 	next();
