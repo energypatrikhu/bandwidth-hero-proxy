ea7313bf1c539e470643ae8bf8e21ebb0d9e02c4
EnergyPatrikHU <energyhun24@gmail.com>
2024-03-23 18:53:02 +0100

    Add cluster support and improve performance

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,36 @@
+import cluster from 'cluster';
 import express from 'express';
-import { cpus } from 'os';
+import { availableParallelism, cpus } from 'os';
 import sharp from 'sharp';
 
 import { paramsParser } from './paramsParser.js';
 import { proxy } from './proxy.js';
 
-sharp.cache(false);
-sharp.simd(true);
-sharp.concurrency(cpus().length);
+const numOfCpus = availableParallelism();
 
-const server = express();
-const port = process.env.PORT || 80;
+if (cluster.isPrimary) {
+	for (let i = 0; i < numOfCpus; i++) {
+		cluster.fork();
+	}
 
-server.get('/', paramsParser, proxy);
-server.get('/favicon.ico', (_req, res) => res.status(204).end());
-server.listen(port, () => console.log(`Listening on ${port}`));
+	cluster.on('exit', (worker) => {
+		console.log(`Worker ${worker.process.pid} died`);
+		cluster.fork();
+	});
+} else {
+	sharp.cache(false);
+	sharp.simd(true);
+	sharp.concurrency(cpus().length);
 
-setInterval(function () {
-	if (gc) gc();
-	else if (global.gc) global.gc();
-}, 5000);
+	const server = express();
+	const port = process.env.PORT || 80;
+
+	server.get('/', paramsParser, proxy);
+	server.get('/favicon.ico', (_req, res) => res.status(204).end());
+	server.listen(port, () => console.log(`Listening on ${port}`));
+
+	setInterval(function () {
+		if (gc) gc();
+		else if (global.gc) global.gc();
+	}, 5000);
+}
