c2de97307dfc09fe935ffabc3701551d5563b9b5
EnergyPatrikHU <energyhun24@gmail.com>
2024-04-27 01:14:16 +0200

    Remove sleep.ts file

--- src/sleep.ts
+++
@@ -1,7 +0,0 @@
-export default (msMin: number, msMax?: number): Promise<void> => {
-	const timeout =
-		Math.abs(msMin) +
-		(!!msMax ? Math.floor(Math.random() * Math.abs(msMax - msMin + 1)) : 0);
-
-	return new Promise((resolve) => setTimeout(resolve, timeout));
-};
