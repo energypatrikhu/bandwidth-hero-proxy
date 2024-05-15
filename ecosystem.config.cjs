b1993d4d306b540059b31960ee09c9e3456f0994
EnergyPatrikHU <energyhun24@gmail.com>
2024-05-15 13:17:36 +0200

    Update node_args

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
 		{
 			name: 'bandwidth-hero-proxy',
 			script: 'build/index.js',
-			node_args: '--expose-gc --max-old-space-size=128',
+			node_args:
+				'--expose-gc --max-old-space-size=128 --jitless --no-opt --no-concurrent-recompilation --noexpose_wasm --max-semi-space-size=4',
 		},
 	],
 };
