1b7a7761e0851e82d389d0ee6b90182805bb7cd8
EnergyPatrikHU <energyhun24@gmail.com>
2024-09-22 11:57:40 +0200

    Fix nested object logging

--- src/beautify-object.ts
+++ src/beautify-object.ts
@@ -3,10 +3,10 @@ function beautifyData(data: any, level: number = 0) {
 
   for (const [key, value] of Object.entries(data)) {
     if (typeof value === 'object') {
+      if (level !== 0) output.push(`${'  '.repeat(level - 1)}> ${key}:`);
       output.push(...beautifyData(value, level + 1));
     } else {
-      if (level === 0) output.push(`\n> ${key}: ${value}`);
-      else output.push(`${'  '.repeat(level)}> ${key}: ${value}`);
+      output.push(`${'  '.repeat(level - 1)}> ${key}: ${value}`);
     }
   }
 
