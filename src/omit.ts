7cb504bb76921f88f09d1fb3416161073aef9727
EnergyPatrikHU <energyhun24@gmail.com>
2024-09-22 12:54:18 +0200

    Refactor omit functions to perform case-insensitive key matching

--- src/omit.ts
+++ src/omit.ts
@@ -2,7 +2,7 @@ export function omitIncludes(object: Record<string, any>, omitKeys: string[]): R
   const deepClone = structuredClone(object);
   for (const [key] of Object.entries(deepClone)) {
     for (const omitKey of omitKeys) {
-      if (key.includes(omitKey)) {
+      if (key.toLowerCase().includes(omitKey.toLowerCase())) {
         delete deepClone[key];
       }
     }
@@ -14,7 +14,7 @@ export function omitStartWith(object: Record<string, any>, omitKeys: string[]): 
   const deepClone = structuredClone(object);
   for (const [key] of Object.entries(deepClone)) {
     for (const omitKey of omitKeys) {
-      if (key.startsWith(omitKey)) {
+      if (key.toLowerCase().startsWith(omitKey.toLowerCase())) {
         delete deepClone[key];
       }
     }
@@ -26,7 +26,7 @@ export function omitEndsWith(object: Record<string, any>, omitKeys: string[]): R
   const deepClone = structuredClone(object);
   for (const [key] of Object.entries(deepClone)) {
     for (const omitKey of omitKeys) {
-      if (key.endsWith(omitKey)) {
+      if (key.toLowerCase().endsWith(omitKey.toLowerCase())) {
         delete deepClone[key];
       }
     }
