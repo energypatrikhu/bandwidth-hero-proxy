22c9050c4fba7de1a570c44c14bcd415c205bb8a
EnergyPatrikHU <energyhun24@gmail.com>
2024-12-17 01:58:08 +0100

    Refactor paramsParser to handle query parameters and unknown queries more effectively

--- src/params-parser.ts
+++ src/params-parser.ts
@@ -1,11 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
 
+const queryKeys = ['url', 'jpg', 'bw', 'l'] as const;
+type QueryKeys = (typeof queryKeys)[number];
+
 export default function paramsParser(request: Request, response: Response, next: NextFunction) {
-	if (!request.query.url) {
-		return response.send('bandwidth-hero-proxy');
+	const requestSearchParams = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
+	const searchParamEntries = requestSearchParams.entries();
+
+	const query: Partial<Record<QueryKeys, string>> = {};
+	const unknownQuery: string[] = [];
+
+	for (const [key, value] of searchParamEntries) {
+		if (queryKeys.includes(key as QueryKeys)) {
+			query[key as QueryKeys] = value;
+		} else {
+			unknownQuery.push(value ? `&${key}=${value}` : `&${key}`);
+		}
 	}
 
-	let url = request.query.url;
+	if (unknownQuery.length > 0) {
+		query.url += unknownQuery.join('');
+	}
+
+	if (!query.url) {
+		response.send('bandwidth-hero-proxy');
+		return;
+	}
+
+	let url = query.url!;
 
 	if (Array.isArray(url)) {
 		url = url.join('&url=');
@@ -13,10 +35,10 @@ export default function paramsParser(request: Request, response: Response, next:
 
 	request.app.locals = {
 		url: url.toString().replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://'),
-		format: request.query.jpg !== '1' ? 'webp' : 'jpeg',
-		grayscale: request.query.bw !== '0',
-		quality: parseInt(request.query.l?.toString()!),
+		format: query.jpg !== '1' ? 'webp' : 'jpeg',
+		grayscale: query.bw !== '0',
+		quality: parseInt(query.l?.toString()!),
 	};
 
-	return next();
+	next();
 }
