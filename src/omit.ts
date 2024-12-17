4be1fcc56b305b6f51e0936d688164665f113bd6
EnergyPatrikHU <energyhun24@gmail.com>
2024-12-17 14:16:45 +0100

    Rename omitStartWith to omitEquals and change logic to omit keys that match exactly

--- src/omit.ts
+++ src/omit.ts
@@ -1,8 +1,8 @@
-export function omitStartWith(object: Record<string, any>, omitKeys: string[]): Record<string, any> {
+export function omitEquals(object: Record<string, any>, omitKeys: string[]): Record<string, any> {
 	const deepClone = structuredClone(object);
 	for (const [key] of Object.entries(deepClone)) {
 		for (const omitKey of omitKeys) {
-			if (key.toLowerCase().startsWith(omitKey.toLowerCase())) {
+			if (key.toLowerCase() === omitKey.toLowerCase()) {
 				delete deepClone[key];
 			}
 		}
