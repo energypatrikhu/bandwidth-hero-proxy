9ddb01907514d4d9e4e9d011bba5c49159551888
EnergyPatrikHU <energyhun24@gmail.com>
2024-12-17 02:32:26 +0100

    Remove redundant omit implementations

--- src/omit.ts
+++ src/omit.ts
@@ -1,35 +1,11 @@
-export function omitIncludes(object: Record<string, any>, omitKeys: string[]): Record<string, any> {
-  const deepClone = structuredClone(object);
-  for (const [key] of Object.entries(deepClone)) {
-    for (const omitKey of omitKeys) {
-      if (key.toLowerCase().includes(omitKey.toLowerCase())) {
-        delete deepClone[key];
-      }
-    }
-  }
-  return deepClone;
-}
-
 export function omitStartWith(object: Record<string, any>, omitKeys: string[]): Record<string, any> {
-  const deepClone = structuredClone(object);
-  for (const [key] of Object.entries(deepClone)) {
-    for (const omitKey of omitKeys) {
-      if (key.toLowerCase().startsWith(omitKey.toLowerCase())) {
-        delete deepClone[key];
-      }
-    }
-  }
-  return deepClone;
-}
-
-export function omitEndsWith(object: Record<string, any>, omitKeys: string[]): Record<string, any> {
-  const deepClone = structuredClone(object);
-  for (const [key] of Object.entries(deepClone)) {
-    for (const omitKey of omitKeys) {
-      if (key.toLowerCase().endsWith(omitKey.toLowerCase())) {
-        delete deepClone[key];
-      }
-    }
-  }
-  return deepClone;
+	const deepClone = structuredClone(object);
+	for (const [key] of Object.entries(deepClone)) {
+		for (const omitKey of omitKeys) {
+			if (key.toLowerCase().startsWith(omitKey.toLowerCase())) {
+				delete deepClone[key];
+			}
+		}
+	}
+	return deepClone;
 }
