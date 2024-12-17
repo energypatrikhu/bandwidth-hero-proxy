2e97cc3c75d78bddc47ae9f29773e77aeed0f8dd
EnergyPatrikHU <energyhun24@gmail.com>
2024-12-17 01:57:48 +0100

    Fix favicon path response function

--- src/index.ts
+++ src/index.ts
@@ -9,37 +9,40 @@ import proxy from './proxy.js';
 const numOfCpus = availableParallelism();
 
 if (cluster.isPrimary) {
-  logger('info', `Primary ${process.pid} is running`);
+	logger('info', `Primary ${process.pid} is running`);
 
-  for (let i = 0; i < numOfCpus; i++) {
-    cluster.fork();
-  }
+	for (let i = 0; i < numOfCpus; i++) {
+		cluster.fork();
+	}
 
-  cluster.on('exit', (worker) => {
-    logger('warn', `Worker ${worker.process.pid} died`);
-    cluster.fork();
-  });
+	cluster.on('exit', (worker) => {
+		logger('warn', `Worker ${worker.process.pid} died`);
+		cluster.fork();
+	});
 } else {
-  const server = express();
-  const port = process.env.PORT || 80;
+	const server = express();
+	const port = process.env.PORT || 80;
 
-  let lastRequestTime = Date.now();
-  function updateLastRequestTime(_req: Request, _res: Response, next: NextFunction) {
-    lastRequestTime = Date.now();
-    next();
-  }
+	let lastRequestTime = Date.now();
+	function updateLastRequestTime(_req: Request, _res: Response, next: NextFunction) {
+		lastRequestTime = Date.now();
+		next();
+	}
 
-  server.get('/', paramsParser, updateLastRequestTime, proxy);
-  server.get('/favicon.ico', (_req, res) => res.status(204).end());
-  server.listen(port, () => {
-    logger('info', `Worker ${process.pid} listening on ${port}`);
-  });
+	server.get('/', paramsParser, updateLastRequestTime, proxy);
+	server.get('/favicon.ico', (_req, res) => {
+		res.status(204).end();
+		return;
+	});
+	server.listen(port, () => {
+		logger('info', `Worker ${process.pid} listening on ${port}`);
+	});
 
-  setInterval(() => {
-    if (Date.now() - lastRequestTime < 10 * 1000) return;
-    if (Date.now() - lastRequestTime > 60 * 60 * 1000) return;
+	setInterval(() => {
+		if (Date.now() - lastRequestTime < 10 * 1000) return;
+		if (Date.now() - lastRequestTime > 60 * 60 * 1000) return;
 
-    if (gc) gc();
-    else if (global.gc) global.gc();
-  }, 5000);
+		if (gc) gc();
+		else if (global.gc) global.gc();
+	}, 5000);
 }
