e3fc778db1f1c71a9de4d023a75431f9e7082c3f
EnergyPatrikHU <energyhun24@gmail.com>
2024-12-19 02:17:48 +0100

    Remove Node.js args in Dockerfile and service configurations

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN npm ci --no-audit
 
 COPY build .
 
-ENTRYPOINT [ "node", "--expose-gc", "--max-old-space-size=128", "--max-semi-space-size=4", "index.js" ]
+ENTRYPOINT [ "node", "index.js" ]
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
 		{
 			name: 'bandwidth-hero-proxy',
 			script: 'build/index.js',
-			node_args: '--expose-gc --max-old-space-size=128 --max-semi-space-size=4',
 		},
 	],
 };
