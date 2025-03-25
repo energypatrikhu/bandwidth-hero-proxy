92678b86959b9966cb1ddae2dc17945dc02e32ba
EnergyPatrikHU <energyhun24@gmail.com>
2025-03-25 09:27:26 +0100

    Add omitPartial method

--- src/utils/omit-equals.ts
+++ src/utils/omit-equals.ts
@@ -2,9 +2,24 @@ export function omitEquals(originalObject: Record<string, any>, keysToOmit: stri
 	const clonedObject = structuredClone(originalObject);
 	const lowerCaseKeysToOmit = new Set(keysToOmit.map((key) => key.toLowerCase()));
 
-	for (const key of Object.keys(clonedObject)) {
-		if (lowerCaseKeysToOmit.has(key.toLowerCase())) {
-			delete clonedObject[key];
+	for (const objectKey of Object.keys(clonedObject)) {
+		if (lowerCaseKeysToOmit.has(objectKey.toLowerCase())) {
+			delete clonedObject[objectKey];
+		}
+	}
+
+	return clonedObject;
+}
+
+export function omitPartial(originalObject: Record<string, any>, keysToOmit: string[]): Record<string, any> {
+	const clonedObject = structuredClone(originalObject);
+	const lowerCaseKeysToOmit = new Set(keysToOmit.map((key) => key.toLowerCase()));
+
+	for (const objectKey of Object.keys(clonedObject)) {
+		for (const omitKey of lowerCaseKeysToOmit) {
+			if (objectKey.includes(omitKey)) {
+				delete clonedObject[objectKey];
+			}
 		}
 	}
 
